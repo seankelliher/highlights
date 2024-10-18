87dcaf7eef671156d668f6a902adc45abd9640b0
Sean Kelliher <sbkelliher@icloud.com>
2024-10-18 15:31:09 -0400

    Add GitHub icon.

--- src/components/FooterPart.vue
+++ src/components/FooterPart.vue
@@ -3,7 +3,10 @@
 
 <template>
     <footer>
-        <p>Footer goes here</p>
+        <figure class="git-icon" @click="toggleMenu()">
+            <img src="/images/git-icon.png" alt="icon for Github">
+        </figure>
+        <p>You're welcome to contact me <a href="https://github.com/seankelliher" target="_blank">through GitHub</a>.</p>
     </footer>
 </template>
 
