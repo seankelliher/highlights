0e53724f8ee91b3e3b666f4d778f6661d73bb15b
Sean Kelliher <sbkelliher@icloud.com>
2024-10-19 12:25:26 -0400

    Add 'footer-text' class.

--- src/components/FooterPart.vue
+++ src/components/FooterPart.vue
@@ -6,7 +6,9 @@
         <figure class="git-icon" @click="toggleMenu()">
             <img src="/images/git-icon.png" alt="icon for Github">
         </figure>
-        <p>You're welcome to contact me <a href="https://github.com/seankelliher" target="_blank">through GitHub</a>.</p>
+        <div class="footer-text">
+            <p>You're welcome to contact me <a href="https://github.com/seankelliher" target="_blank">through GitHub</a>.</p>
+        </div>
     </footer>
 </template>
 
