cea1dc58d78a652ec806615b6b8d1a2587632f19
Sean Kelliher <sbkelliher@icloud.com>
2024-10-20 16:00:11 -0400

    Add figcaption. Remove un-needed toggleMenu().

--- src/components/FooterPart.vue
+++ src/components/FooterPart.vue
@@ -3,8 +3,9 @@
 
 <template>
     <footer>
-        <figure class="git-icon" @click="toggleMenu()">
+        <figure class="git-icon">
             <img src="/images/git-icon.png" alt="icon for Github">
+            <figcaption hidden>Octocat icon for GitHub.</figcaption>
         </figure>
         <div class="footer-text">
             <p>You're welcome to contact me <a href="https://github.com/seankelliher" target="_blank">through GitHub</a>.</p>
