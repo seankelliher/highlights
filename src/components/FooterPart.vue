55f9c964d654f6f4069bdc34b91cf218ef834c61
Sean Kelliher <sbkelliher@icloud.com>
2024-10-24 13:02:22 -0400

    Add contact info.

--- src/components/FooterPart.vue
+++ src/components/FooterPart.vue
@@ -1,4 +1,11 @@
 <script setup>
+import { onMounted } from "vue";
+
+onMounted(() => {
+    const address = ("<n uers=\"znvygb:foxryyvure@vpybhq.pbz\" ery=\"absbyybj\">Rznvy zr</n>".replace(/[a-zA-Z]/g, function(c){return String.fromCharCode((c<="Z"?90:122)>=(c=c.charCodeAt(0)+13)?c:c-26);}));
+    const place = document.getElementById("place");
+    place.innerHTML = address;
+});
 </script>
 
 <template>
@@ -8,7 +15,7 @@
             <figcaption hidden>Octocat icon for GitHub.</figcaption>
         </figure>
         <div class="footer-text">
-            <p>You're welcome to contact me <a href="https://github.com/seankelliher" target="_blank">through GitHub</a>.</p>
+            <p><a href="https://github.com/seankelliher" target="_blank">GitHub</a> | <span id="place"></span></p> 
         </div>
     </footer>
 </template>
