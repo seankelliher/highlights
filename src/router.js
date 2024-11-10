72f1fb5adfa3239baee0f607c68a9ce5c22c5304
Sean Kelliher <sbkelliher@icloud.com>
2024-11-10 12:19:18 -0500

    Remove route for each project component. Add single route for dynamic 'projects' template component.

--- src/router.js
+++ src/router.js
@@ -1,29 +1,11 @@
 import { createRouter,createWebHistory } from "vue-router";
 import HomePage from "./components/HomePage.vue";
-import UsNationalParksAlerts from "./components/UsNationalParksAlerts.vue";
-import GlobalFertilityRates from "./components/GlobalFertilityRates.vue";
-import UsSenateDirectory from "./components/UsSenateDirectory.vue";
-import CaliforniaTempsPrecip from "./components/CaliforniaTempsPrecip.vue";
-import NycShootingIncidents from "./components/NycShootingIncidents.vue";
-import MapCoordinatesDms from "./components/MapCoordinatesDms.vue";
-import NycFirehouses from "./components/NycFirehouses.vue";
-import NycPublicPools from "./components/NycPublicPools.vue";
-import MtWashingtonTemps from "./components/MtWashingtonTemps.vue";
-import ImgGallery from "./components/ImgGallery.vue";
+import ProjectsTemplate from "./components/ProjectsTemplate.vue";
 import NotFound from "./components/NotFound.vue";
 
 const routes = [
     { path: "/", component: HomePage },
-    { path: "/us-national-parks-alerts", component: UsNationalParksAlerts },
-    { path: "/global-fertility-rates", component: GlobalFertilityRates },
-    { path: "/us-senate-directory", component: UsSenateDirectory },
-    { path: "/california-temps-precip", component: CaliforniaTempsPrecip },
-    { path: "/nyc-shooting-incidents", component: NycShootingIncidents },
-    { path: "/map-coordinates-dms", component: MapCoordinatesDms },
-    { path: "/nyc-firehouses", component: NycFirehouses },
-    { path: "/nyc-public-pools", component: NycPublicPools },
-    { path: "/mt-washington-temps", component: MtWashingtonTemps },
-    { path: "/img-gallery", component: ImgGallery },
+    { path: "/projects/:id", component: ProjectsTemplate },
     { path: "/:catchAll(.*)", component: NotFound }
 ];
 
