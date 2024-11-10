2151bd8b9c037fd4ceac772dbe139a60549e9702
Sean Kelliher <sbkelliher@icloud.com>
2024-11-10 12:17:26 -0500

    Tweaks to URL structure. Needed, projects now in 'projects' directory.

--- src/composables/repo-links.js
+++ src/composables/repo-links.js
@@ -1,16 +1,19 @@
 const links = {
     openGitHub(repo) {
-        window.open(`https://github.com/seankelliher${repo}`);
+        const clean = repo.substring(9); // removes /projects
+        window.open(`https://github.com/seankelliher${clean}`);
     },
     openNetlify(repo) {
-        const clean = repo.substring(1); // removes preceding slash.
+        const clean = repo.substring(10); // removes /projects
         window.open(`https://sean-kelliher-${clean}.netlify.app`);
     },
     openHerokuSimple(repo) {
-        window.open(`https:/${repo}.herokuapp.com`);
+        const clean = repo.substring(9); // removes projects/
+        window.open(`https:/${clean}.herokuapp.com`);
     },
     openHerokuComplex(repo, id) {
-        window.open(`https:/${repo}-${id}.herokuapp.com/`);
+        const clean = repo.substring(9); // removes projects/
+        window.open(`https:/${clean}-${id}.herokuapp.com/`);
     }
 };
 
