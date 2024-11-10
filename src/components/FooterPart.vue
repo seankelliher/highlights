7e10eb52771bf8bdbf72c5dbce594fe1f24643bf
Sean Kelliher <sbkelliher@icloud.com>
2024-11-10 12:14:36 -0500

    Add Google fonts. Text change to 'email' button.

--- src/components/FooterPart.vue
+++ src/components/FooterPart.vue
@@ -2,7 +2,7 @@
 import { onMounted } from "vue";
 
 onMounted(() => {
-    const address = ("<n uers=\"znvygb:foxryyvure@vpybhq.pbz\" ery=\"absbyybj\">Rznvy zr</n>".replace(/[a-zA-Z]/g, function(c){return String.fromCharCode((c<="Z"?90:122)>=(c=c.charCodeAt(0)+13)?c:c-26);}));
+    const address = ("<n uers=\"znvygb:foxryyvure@vpybhq.pbz\" ery=\"absbyybj\">Rznvy</n>".replace(/[a-zA-Z]/g, function(c){return String.fromCharCode((c<="Z"?90:122)>=(c=c.charCodeAt(0)+13)?c:c-26);}));
     const place = document.getElementById("place");
     place.innerHTML = address;
 });
@@ -10,10 +10,13 @@ onMounted(() => {
 
 <template>
     <footer>
-        <button id="place">
-            Email me
+        <button
+            id="place"
+            class="roboto-serif-four"
+        >
+            Email
         </button>
-        <button>
+        <button class="roboto-serif-four">
             <a href="https://github.com/seankelliher" target="_blank">GitHub</a>
         </button>
     </footer>
