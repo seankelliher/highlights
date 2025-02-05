1c088ff088dd736143611f8d506c01242f7fc205
Sean Kelliher <sbkelliher@icloud.com>
2025-02-05 11:26:45 -0500

    Add font class.

--- src/components/NotFound.vue
+++ src/components/NotFound.vue
@@ -3,7 +3,7 @@
 
 <template>
     <div class="error-text">
-        <p>Sorry, the page you're looking for was not found - error 404.</p>
+        <p class="noto-sans-four">Sorry, the page you're looking for was not found - error 404.</p>
     </div>
 </template>
 
