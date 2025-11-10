ae7e3e6e6e17fefd1605510ec46cb38cc8d11791
Sean Kelliher <sbkelliher@icloud.com>
2025-11-09 20:12:24 -0500

    Modify fonts in h2 and dt. Remove cute graphics.

--- src/components/ProjectsTemplate.vue
+++ src/components/ProjectsTemplate.vue
@@ -22,7 +22,7 @@ document.querySelector("meta[name='description']").setAttribute("content", proje
     <div class="intro">
         <!-- Primary headlines -->
         <hgroup>
-            <h2 class="bevan-regular">{{ projectContent.headline }}</h2>
+            <h2 class="noto-sans-eight">{{ projectContent.headline }}</h2>
             <h3 class="noto-sans-four">{{ projectContent.subHeadline }}</h3>
         </hgroup>
 
@@ -48,73 +48,52 @@ document.querySelector("meta[name='description']").setAttribute("content", proje
             <div class="intro-sidebar-card">
                 <div class="intro-sidebar-text">
                     <dl>
-                        <dt class="noto-sans-seven">Working version</dt>
+                        <dt class="noto-sans-eight">Working version</dt>
                         <dd class="noto-sans-four">
                             <a
                                 :href="`https://${projectContent.platformUrl}`"
                                 target="_blank"
                                 class="blue"
                             >
-                                View working version on {{ projectContent.platform }}
+                                View on {{ projectContent.platform }}
                             </a> <span v-if="projectContent.platformWarn">{{ projectContent.platformWarn }}</span>
                         </dd>
                     </dl>
                 </div>
-                <figure class="intro-sidebar-image">
-                    <img
-                        src="/images/avatars/avatar-gears.jpg"
-                        alt="cute colorful gear"
-                    >
-                    <figcaption hidden>avatar, machinery working</figcaption>
-                </figure>
             </div>
 
             <!-- GitHub card -->
             <div class="intro-sidebar-card">
                 <div class="intro-sidebar-text">
                     <dl>
-                        <dt class="noto-sans-seven">Source code</dt>
+                        <dt class="noto-sans-eight">Source code</dt>
                         <dd class="noto-sans-four">
                             <a
                                 :href="`https://github.com/seankelliher/${projectContent.id}`"
                                 target="_blank"
                                 class="blue"
                             >
-                                View source code on GitHub
+                                View on GitHub
                             </a>
                         </dd>
                     </dl>
                 </div>
-                <figure class="intro-sidebar-image">
-                    <img
-                        src="/images/avatars/avatar-discs.jpg"
-                        alt="cute colorful discs"
-                    >
-                    <figcaption hidden>avatar, discs for storage</figcaption>
-                </figure>
             </div>
             <!-- Technologies card -->
             <div class="intro-sidebar-card">
                 <div class="intro-sidebar-text">
                     <dl>
-                        <dt class="noto-sans-seven">Technologies</dt>
+                        <dt class="noto-sans-eight">Technologies</dt>
                         <dd class="noto-sans-four">{{ projectContent.techs }}</dd>
                     </dl>
                 </div>
-                <figure class="intro-sidebar-image">
-                    <img
-                        src="/images/avatars/avatar-dials.jpg"
-                        alt="cute colorful dials and buttons"
-                    >
-                    <figcaption hidden>avatar, dials and buttons for technology</figcaption>
-                </figure>
             </div>
         </div>
     </div>
     <!-- Wireframes -->
     <main>
         <hgroup>
-            <h2 class="bevan-regular">Wireframes</h2>
+            <h2 class="noto-sans-eight">Wireframes</h2>
             <h3 class="noto-sans-four">The interface was built based on these initial drawings</h3>
         </hgroup>
         <div class="wireframes">
